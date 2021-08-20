3abbba3caa3caeb6d78137c89f4f7f4ab538ce45
Tom Durrant <tomchdurrant@gmail.com>
2021-08-20 22:22:25 +0100

    Start of use context

--- src/exercise/03.js
+++ src/exercise/03.js
@@ -4,6 +4,11 @@
 import * as React from 'react'
 
 // 🐨 create your CountContext here with React.createContext
+const CountContext = React.createContext()
+function CountProvider(props) {
+  const [count, setCount] = React.useState(0)
+  return <CountContext.Provider value={[count, setCount]} {...props} />
+}
 
 // 🐨 create a CountProvider component here that does this:
 //   🐨 get the count state and setCount updater with React.useState
@@ -13,13 +18,14 @@ import * as React from 'react'
 
 function CountDisplay() {
   // 🐨 get the count from useContext with the CountContext
-  const count = 0
+  const [count, ] = React.useContext(CountContext)
+  // const count = 0
   return <div>{`The current count is ${count}`}</div>
 }
 
 function Counter() {
   // 🐨 get the setCount from useContext with the CountContext
-  const setCount = () => {}
+  const [, setCount] = React.useContext(CountContext)
   const increment = () => setCount(c => c + 1)
   return <button onClick={increment}>Increment count</button>
 }
@@ -27,12 +33,10 @@ function Counter() {
 function App() {
   return (
     <div>
-      {/*
-        🐨 wrap these two components in the CountProvider so they can access
-        the CountContext value
-      */}
-      <CountDisplay />
-      <Counter />
+      <CountProvider>
+        <CountDisplay />
+        <Counter />
+      </CountProvider>
     </div>
   )
 }
